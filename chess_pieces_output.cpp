8b4421f14b7ac21b1c44619c59f81b2ae6a73768
TymoteuszChatys <60819253+TymoteuszChatys@users.noreply.github.com>
2020-03-25 18:49:53 +0000

    first unicode test

---
+++ chess_pieces_output.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+int main() {
+    std::cout << "King   \u265A , \u2654" << std::endl;
+    std::cout << "Queen  \u265B , \u2655" << std::endl;
+    std::cout << "Rook   \u265C , \u2656" << std::endl;
+    std::cout << "Bishop \u265D , \u2657" << std::endl;
+    std::cout << "Knight \u265E , \u2658" << std::endl;
+    std::cout << "Pawn   \u265F , \u2659" << std::endl;
+}
