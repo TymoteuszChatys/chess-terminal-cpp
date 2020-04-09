0c146437c8a4c1a81ccbabf1881c5f01829b9c98
tymoteusz <tymoteuszchatys@gmail.com>
2020-04-09 21:23:57 +0100

    correct output

--- First_code.cpp
+++ First_code.cpp
@@ -27,22 +27,31 @@ namespace chess
         friend class piece;
         friend std::ostream &operator<<(std::ostream &out_stream, const board &the_board);
     protected:
-        int height{8};
-        int width{8};
         int* board_representation{nullptr};
-        int size{(height+4)*(width+2)};
+        int size{10*12};
     public:
         //default constructor, starting position.
         board() {
             board_representation = new int[size]{};
-            std::string starting_fen{"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"};
+            populate_out_of_range();
+            std::string starting_fen{"rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2"};
             position(starting_fen);
         }
         virtual ~board() {delete[] board_representation;};    
         size_t get_size() const{return size;}
+        void populate_out_of_range();
         int position(std::string FEN);
 
     };
+
+    void board::populate_out_of_range(){
+        for (size_t index{}; index<get_size(); index++){
+            if ((index >= 0 && index <= 20) || index >= 99 || (index-1)%10 == 8 || (index-1)%10 == 9) {
+                std::cout << "hi" << std::endl;
+                board_representation[index] = 7;
+            }
+        }
+    }
 
     int board::position(std::string FEN){
         std::vector<std::string> splitted_fen = split(FEN,' ');
@@ -63,13 +72,14 @@ namespace chess
             else if(squares_64<16){square = squares_64 + 23;}
             else if(squares_64<24){square = squares_64 + 25;}
             else if(squares_64<32){square = squares_64 + 27;}
-            else if(squares_64<40){square = squares_64 + 31;}
-            else if(squares_64<48){square = squares_64 + 33;}
-            else if(squares_64<56){square = squares_64 + 35;}
-            else if(squares_64<64){square = squares_64 + 37;}
+            else if(squares_64<40){square = squares_64 + 29;}
+            else if(squares_64<48){square = squares_64 + 31;}
+            else if(squares_64<56){square = squares_64 + 33;}
+            else if(squares_64<64){square = squares_64 + 35;}
             switch (letter)
             {
-                case 'p' : board_representation[square] = -1; break;
+                case 'p' : board_representation[square] = -1; 
+                king (1,square); break;
                 case 'n' : board_representation[square] = -2; break;
                 case 'b' : board_representation[square] = -3; break;
                 case 'r' : board_representation[square] = -4; break;
@@ -127,24 +137,25 @@ namespace chess
         ~king(){}
 
         std::string name(){return "king";}
-        int get_colour(){return colour;
-        }
+        int get_colour(){return colour;}
     };
 
 
     std::ostream &operator<<(std::ostream &out_stream, const board &the_board)
     {
     if(the_board.get_size() > 0){
-        out_stream << "[";
-        for (size_t index{0}; index < the_board.get_size(); index++) {
-        if (index == the_board.get_size() - 1) {
-            out_stream << the_board.board_representation[index] << "]";
-        }else{
-            out_stream << the_board.board_representation[index] << ";";
+        for (size_t mirror_index{the_board.get_size()/10}; mirror_index > 0; mirror_index--){
+            for (size_t index{(mirror_index-1)*10}; index < mirror_index*10; index++) {
+                if ((index+1) % 10 == 0) {
+                    out_stream << the_board.board_representation[index] << std::endl;
+                }else{
+                    out_stream << the_board.board_representation[index] << ";";
+                }
+            }
         }
-        }
+            
     }else{
-        out_stream << "vector is empty" << std::endl;
+        out_stream << "board is empty" << std::endl;
     }
     return out_stream;
     }
@@ -159,10 +170,8 @@ namespace chess
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
     chess::board main_board;
     int size = main_board.get_size();
-    std::cout << size << std::endl;
     std::cout << main_board << std::endl;
     return 0;
 }
